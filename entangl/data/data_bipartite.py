e12eaa505d8831207f63535be1f7e3e56c909fff
FS <Frederic.sauv@gmail.com>
2018-11-02 10:27:36 +0000

    Complete (partially) the data module

--- entangl/data/data_bipartite.py
+++ entangl/data/data_bipartite.py
@@ -6,48 +6,113 @@ Created on Sat Aug 04 22:18:20 2018
 
 Generate datasets for ANN training testing.
 
-TODO:
-TODO:
-TODO:
+TODO: Ensure compatibility with ANN scripts
+TODO: Look at shape of Nuke's input file
+
 """
+from ..utility import bipartite
 
-
-def gen_dataset(states, nb_examples, nb_measures):
-    """ Generate data_set (both input and ouputs) based on a list of state vectors.
-    data_set[i] = (X_A, Y_A, Z_A, X_B, Y_B, Z_B, E) 
+def gen_bipartite_dataset(nb_train, nb_test, nb_m, e_min = 0, e_max = np.log(2),
+                          systems = ['A', 'B'], check_entanglement = False):
+    """ Generate data_set of measures based on random states of uniformly 
+    distributed entropies
+    data_set[i] = (X_A, Y_A, Z_A, X_B, Y_B, Z_B, E, ) 
 
 	Arguments
 	---------
-		nb_examples: int
-			how many examples do we want to generate
-		nb_measures: int
-			How many measurements are doing
+		nb_training: int
+			how many training examples do we want
+		nb_testing: int
+            how many testing examples do we want
+         nb_m: int
+			How many measurements are we doing
+         e_min: float
+             Min entropy
+         e_max: float
+             Max entropy
+         check_entanglement:
+            Verify that entanglement required is the same as the one obtained
 
 	Output
 	------
-
+        data_set: (data_train, data_test, infos)
+            data_train: (nb_train, ) 
+            data_test: (nb_test, )
+            info_fileds: str 
+                Provides information about the columns and parameters used to 
+                generate the data
     """
-    lambdas = rdm_lambda_gen(nb_examples)
+    info = []
+    nb_total = nb_train + nb_test
+    ent = np.random.uniform(e_min, e_max, nb_total)
+    lambd = bipartite.ent_to_lambda(ent)
+    states = bipartite.rdm_states_from_lambda(lambd)
+    if check_entanglement:
+        ent_final = bipartite.entangl_of_states(states)
+        assert np.allclose(ent_final, ent), "Entanglement produced don't match"
+    x_A = measurements(states, nb_m, [1,0,0], 'A')
+    y_A = measurements(states, nb_m, [0,1,0], 'A')
+    z_A = measurements(states, nb_m, [0,0,1], 'A')
+    x_B = measurements(states, nb_m, [1,0,0], 'B')
+    y_B = measurements(states, nb_m, [0,1,0], 'B')
+    z_B = measurements(states, nb_m, [0,0,1], 'B')
+    
+    data_set = np.stack((x_A, y_A, z_A, x_B, y_B, z_B, ent, states), axis=1)
+    i = np.random.shuffle(np.arange(nb_total)) 
+    i_train = i[:nb_train]
+    i_test = i[nb_train:nb_total]
+    info = []
+    info.append("# Array shape: {0}".format(data_set.shape))
+    info.append("# Number of measuremets: {0} ".format(nb_m))
+    info.append("# Ent min/max {0}/{1}".format(e_min, e_max))
+    info.append("# Columns: (0-2) X, Y, Z measurements on A")
+    info.append("#(3-5) X, Y, Z measurements on B")
+    info.append("#(6-10) amplitudes of states (in the computational basis)")
 
-    x1 = measurements(states, nb_measures, [1,0,0], 'A')
-    y1 = measurements(states, nb_measures, [0,1,0], 'A')
-    z1 = measurements(states, nb_measures, [0,0,1], 'A')
-    x2 = measurements(states, nb_measures, [1,0,0], 'B')
-    y2 = measurements(states, nb_measures, [0,1,0], 'B')
-    z2 = measurements(states, nb_measures, [0,0,1], 'B')
-    
-    ents = vn_ent(partial_trace(dm(state_vectors)))
-    ents = ents.reshape((len(state_vectors), 1))
-    ents = np.pad(ents, (0,2), mode = 'constant')[:-2] # add zeros to make shape compatible with measurements x1,x2...
-    
-    data_set = np.stack((x1,y1,z1,x2,y2,z2,ents), axis=1)
-    
-    return data_set0
+    return data_set[i_train, :], data_set[i_testing, :], info
 
+def write_data_set(data_set, file_name, folder_name=None):
+    """ Write a data_set as a csv file
+    Input
+    -----
+        data_set
+        
+    Output
+    ------
+        csv file, with some comments
+        
+    """
+    data_train, data_test, info = data_set
+    
+    with file(file_name, 'w+') as outfile:
+        outfile.write(infos)
+        for data_slice in data_set:
+            np.savetxt(outfile, data_slice, fmt='%5.12g') #'%-7.2f' '%.4e'
+            #outfile.write('# New vector with column format (xa, ya, za, xb, yb, zb, vne) \n')
+
+
+def load_data_set(data_set, file_name, folder_name=None):
+    """ Write a data_set as a csv file
+    Input
+    -----
+    
+    Output
+    ------
+        data_set: (data_train, data_test, infos)
+            With the same characteristics as the output of 
+    """
+    
+    
+
+def extend_dataset_symmetries(dataset):
+    """ Create an extended dataset using the symmetries of the system:
+    
+    """
+    pass
 
 if __name__ == '__main__':
 	test_creating = False
-	test_loading
+	test_loading = False
 
 	if(test_creating):
 	# script to generate data
