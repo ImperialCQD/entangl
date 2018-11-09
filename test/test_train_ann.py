4267061f7f8b3d4a51c30113a7da0aeab8570a44
Fred <Sauvage>
2018-11-09 09:26:48 +0000

    Polish test_train_script

--- test/test_train_ann.py
+++ test/test_train_ann.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  8 23:56:43 2018
-
-@author: FS
+@author0: MIUKE
+@author1: FS
 """
 import sys
 sys.path.append('../../entangl')
@@ -12,22 +12,18 @@ import numpy as np
 import matplotlib.pylab as plt
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-# import keras.optimizers as ko
-# from keras.datasets import mnist
-# from keras.utils import np_utils
-# import keras.initializers
-# from keras.callbacks import EarlyStopping, ModelCheckpoint
-
 from entangl.data import data_bipartite as data
-
 
 
 ## Getting the data
 (x_train, y_train, st_train), (x_test, y_test, st_test) = data.load_data_set('perfect_10k2k', states = True)
 x_train = x_train[:, :3]
 x_test = x_test[:, :3]
-y_train /= np.log(2) # normalization
-y_test /= np.log(2) # normalization
+y_train /= np.log(2) 
+y_test /= np.log(2) 
+plt.hist(y_train)
+plt.hist(y_test)
+
 
 ##Defining the ANN
 batch_size = 100
@@ -39,7 +35,6 @@ hl2_neurons = 30
 hl3_neurons = 30
 initializer='normal'
 #initializer = keras.initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None)  
-
 model.add(Dense(units=hl_neurons, input_dim=input_neurons, kernel_initializer=initializer))
 model.add(Activation('elu'))
 #model.add(Dropout(0.2))
@@ -53,58 +48,42 @@ model.add(Activation('linear'))
 model.compile(optimizer='adadelta',loss='mse', metrics=['mae'])
 model.summary()
 
+
+## Training it
 history = model.fit(x_train, y_train, epochs=nb_epoch, batch_size=batch_size, verbose=1)
 
-
 ## Evaluating the ANN
-evaluation_train = model.evaluate(x_train, y_train, verbose=1)
-print("On training data:")
+evaluation_train = model.evaluate(x_train, y_train)
+print("On training data [mse, mae]:")
 print(evaluation_train)
 
-evaluation = model.evaluate(x_test, y_test, verbose=1)
-print("On test data:")
+evaluation = model.evaluate(x_test, y_test)
+print("On test data [mse, mae]:")
 print(evaluation)
 
 
-nn_tests = model.predict(x_test, batch_size=len(x_test))   # predicted entanglement values
-nn_tests = nn_tests.reshape((len(nn_tests),))
-actual_ys = y_test                                         # actual
-msd = np.abs((nn_tests - actual_ys))
-spread=np.average(msd)
-spread2 = msd * msd  #spread squared
-std = np.sqrt(np.abs(np.average(spread2) - spread*spread))
+## Visualization 
+# 1: mae errors; 
+# 2: hist of mae
+# 3 & 4: weights of layer 1 visualized 
+y_pred = np.squeeze(model.predict(x_test))   
+err = np.abs(y_pred - y_test)
+err_avg = np.average(err)
+err_std = np.std(err)
+y_pred_train = np.squeeze(model.predict(x_train))
+err_train = np.abs(y_pred_train - y_train)
+err_avg_train = np.average(err_train)
+err_std_train = np.std(err_train)
 
-
-
-## Visualization 1: mae errors; 2: hist of mae; 3 & 4: weights of layer 1 visualized 
-import matplotlib.pyplot as plt
 plt.figure(1)
-plt.plot(actual_ys, 
-         msd, '.', label='mean absolute error test {0} std {1} Mean absolute error train {2} \n'.format(
-        round(evaluation[1],3),round(std, 3), round(evaluation_train[1],3) ))
-plt.xlabel('VN entanglement values')
-plt.ylabel('Mean absolute error of NN vnes')
+label1='avg(std) abs error - test: {:.3f}({:.3f}), \n train {:.3f} ({:.3f})'.format(
+         err_avg, err_std, err_avg_train, err_std_train)
+plt.plot(y_test, err, '.', label = label1)
+plt.xlabel('Real entanglement values')
+plt.ylabel('Abs error of')
 plt.legend()
+
 plt.figure(2)
-plt.hist(msd, bins=100)
+plt.hist(err, bins=100)
 plt.show(2)
 
-x = model.get_weights()
-plt.figure(3)
-counter=0      
-for i in range(hl_neurons):
-    plt.plot(range(1,input_neurons+1), x[0][:,counter],'x-',label='HL neuron {0}'.format(counter+1))
-    counter+=1
-plt.xlabel('weights for 10 neurons in first hidden layer')
-plt.ylabel('weights for 10 neurons in first hidden layer')
-plt.xlabel('Input neuron number')
-plt.legend()
-
-plt.figure(4)
-counter=0      
-for i in range(input_neurons):
-    plt.plot(range(1,hl_neurons+1), x[0][counter],'x-',label='Input neuron {0}'.format(counter+1))
-    counter+=1
-plt.ylabel('weights for 10 neurons in first hidden layer')
-plt.xlabel('HL neuron number')
-plt.legend()
